9bec24fcaca67ea267ed2ace8fdd0ce30c5d68e6
RyanSecrest <ryan.secrest@wsu.edu>
2024-11-09 17:28:31 -0800

    first commit

---
+++ FetchRecipe/FetchRecipe/FetchRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeApp.swift
+//  FetchRecipe
+//
+//  Created by Ryan Secrest on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
